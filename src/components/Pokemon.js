dfe580021849ce36efe0c4de32e22bd8bf1d6ecb
astergabedev <gabiferpub@gmail.com>
2023-02-23 09:47:40 -0300

    pokemon cards plus a few changes

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,16 +2,38 @@ import React from "react";
 
 const Pokemon = (props) => {
   const { pokemon } = props;
+  const onHeartClick = () => {
+    console.log("pode favoritar");
+  };
+  const heart = "❤️";
   return (
     <div className="pokemon-card">
       <div className="pokemon-image-container">
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <img
+          className="pokemon-image"
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+        />
       </div>
       <div className="card-body">
-        <div className="pokemon-type">
-          <div>#{pokemon.type}</div>
+        <div className="card-top">
+          <h3>{pokemon.name}</h3>
+          <div>#{pokemon.id}</div>
         </div>
-        <div className="card-botton"></div>
+        <div className="card-botton">
+          <div className="pokemon-type">
+            {pokemon.types.map((type, index) => {
+              return (
+                <div key={index} className="pokemon-type-name">
+                  {type.type.name}
+                </div>
+              );
+            })}
+          </div>
+          <button className="pokemon-heart-btn" onClick={onHeartClick}>
+            {heart}
+          </button>
+        </div>
       </div>
     </div>
   );
